7fee92371be4d062c49ac4b3b916c6bcfe4a0b73
Lyle Scott, III <lyle@digitalfoo.net>
2016-10-28 22:43:39 -0400

    add lambda

---
+++ lambda_function.py
@@ -0,0 +1,73 @@
+# Lyle Scott, III
+# https://ls3.io
+# lyle@ls3.io
+
+from __future__ import print_function
+from datetime import datetime
+import re
+
+# Fix up PYTHONPATH to account for libs and src directories.
+import sys
+sys.path.insert(0, 'libs')
+sys.path.insert(1, 'src')
+
+import requests
+
+# Local package imports.
+from helpers import build_response, build_speechlet_response
+import settings
+
+
+def lambda_handler(event, context):
+    """Route the incoming request based on the event."""
+
+    # Prevent unwanted access to this Lambda function.
+    app_id = event['session']['application']['applicationId']
+    if settings.LAMBDA_ARN and app_id != settings.LAMBDA_ARN:
+        speech = 'Permission denied!'
+        build_response(build_speechlet_response(speech))
+
+    func_map = {
+        'LaunchRequest': on_launch,
+        'IntentRequest': on_intent,
+    }
+
+    request = event['request']
+
+    return func_map[request['type']](request, event['session'])
+
+
+def on_launch(request, session):
+    """Called when the user launches the skill without specifying what they want."""
+    return intent_next_meeting()
+
+
+def on_intent(request, session):
+    """Called when the user specifies an intent for this skill."""
+    if request['intent']['name'] == "NextMeetupIntent":
+        return intent_next_meeting()
+
+
+def intent_next_meeting():
+    """Initialize the session to have some attributes."""
+    url = '{}/events/?key={}'.format(settings.MEETUP_API_ROOT, settings.MEETUP_API_KEY)
+    response = requests.get(url)
+    try:
+        response_json = response.json()
+    except ValueError:
+        speech = 'Oh, no! Invalid JSON response.'
+        return build_response(build_speechlet_response(speech))
+
+    if not len(response_json):
+        speech = 'There are currently no scheduled meetups coming up, but check back soon!'
+        return build_response(build_speechlet_response(speech))
+
+    event = response_json[0]
+    name = event['name']
+    when = datetime.fromtimestamp(event['time'] / 1000).strftime('%A, %B %d at %I:%M %p')
+    desc = re.sub('<[^<]+?>', '', event['description'])
+    speech = ('The next Python Saint Pete meeting is {}. The topic is {} with a description of '
+              '{}'.format(when, name, desc))
+
+    return build_response(build_speechlet_response(
+        speech, card_title='PyStPete Meetup', card_content='\n'.join((when, name, desc))))
---
+++ settings.dist.py
@@ -0,0 +1,13 @@
+
+# Your Meetup URL: https://www.meetup.com/Saint-Petersburg-Python-Meetup/
+MEETUP_URL_NAME = 'Saint-Petersburg-Python-Meetup'
+
+# The API token: https://secure.meetup.com/meetup_api/key/
+MEETUP_API_KEY = ''
+
+# The ARN of your Lambda function to lock down who can execute the Amazon Echo.
+# If not set, any ARN will be accepted.
+LAMBDA_ARN = ''
+
+# Root URL. Nothing to really change with this.
+MEETUP_API_ROOT = 'https://api.meetup.com'
